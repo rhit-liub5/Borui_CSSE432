fde5033f19f2c5017d04b7881b4348f8039c2d3e
rhit-liub5 <95311554+rhit-liub5@users.noreply.github.com>
2026-03-20 14:52:07 -0400

    Finish Socket demo

---
+++ Socket/Client.py
@@ -0,0 +1,22 @@
+import socket
+import sys
+
+def client_program():
+    if(len(sys.argv) != 3):
+        print("Usage: python Socket_example.py <host> <port>")
+        sys.exit()
+    
+    server_port = int(sys.argv[2]) ##获取端口号
+    server_addr = (sys.argv[1], server_port) ##获取服务器地址和端口号
+    
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) ##创建套接字
+    client_socket.connect(server_addr) ##连接到服务器
+    message = input("Enter message to send to server: ") ##获取用户输入
+    while message.lower().strip() != ';;;':
+        client_socket.send(message.encode()) ##发送数据
+        data = client_socket.recv(1024).decode() ##接收数据
+        print("Received from server: ", str(data)) ##打印服务器响应
+        message = input("Enter message to send to server: ") ##获取用户输入
+    client_socket.close() ##关闭套接字
+if __name__ == '_main_':
+    client_program()
---
+++ Socket/Server.py
@@ -0,0 +1,38 @@
+import socket
+import sys
+
+def server_program():
+    host = socket.gethostname() ##获取本地主机名
+    host_ip = socket.gethostbyname(host) ##获取本地主机IP地址
+    
+    if(len(sys.argv) != 2):
+        print("Usage: python Socket_example.py <port>")
+        sys.exit()
+    port = int(sys.argv[1]) ##获取端口号
+    
+    print("Host Name: ", str(host), "IP: ", str(host_ip), "Port: ", str(port))
+    
+    server_listening_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) ##创建套接字
+    
+    server_listening_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) ##设置套接字选项，允许重用地址
+    
+    server_listening_socket.bind(('', port)) ##绑定套接字到地址和端口
+    
+    server_listening_socket.listen(5) ##开始监听连接，参数为最大连接数
+    
+    while True:
+        server_comm_socket, address = server_listening_socket.accept() ##接受连接
+        
+        print("Connection from: ", str(address))
+        
+        while True:
+            incoming_data = server_comm_socket.recv(1024).decode() ##接收数据
+            
+            if not incoming_data:
+                break
+            print("Received from client: ", str(incoming_data))
+            outgoing_data = str(incoming_data).upper() ##获取用户输入作为响应
+            server_comm_socket.send(outgoing_data.encode()) ##发送数据
+        server_comm_socket.close() ##关闭通信套接字
+if __name__ == '__main__':
+    server_program()
