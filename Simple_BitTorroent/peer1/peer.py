e8005c0a30a0f4280c4e7a365dbe9645973cf9ef
rhit-liub5 <95311554+rhit-liub5@users.noreply.github.com>
2026-05-18 13:38:10 -0400

    get server and client

--- Simple_BitTorroent/peer1/peer.py
+++ Simple_BitTorroent/peer1/peer.py
@@ -1,11 +1,22 @@
 import socket
 import os
 import sys
+import threading
 
 
 TRACKER_HOST = "127.0.0.1"
 TRACKER_PORT = 9000
 
+def recv_all(conn):
+    data = b""
+    while True:
+        chunk = conn.recv(1)
+        if not chunk:
+            break
+        data += chunk
+        if chunk == b"\n":
+            break
+    return data.decode().strip()
 
 def find_files(folder):
     filenames = []
@@ -21,7 +32,6 @@ def find_files(folder):
 
     return filenames
 
-
 def send_register(peer_id, peer_ip, peer_port, shared_folder):
     filenames = find_files(shared_folder)
 
@@ -30,13 +40,73 @@ def send_register(peer_id, peer_ip, peer_port, shared_folder):
         return
 
     command = f"REGISTER {peer_id} {peer_ip} {peer_port} " + " ".join(filenames) + "\n"
-
+    ## with 的好处是：用完之后会自动关闭 socket。
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect((TRACKER_HOST, TRACKER_PORT))
         s.sendall(command.encode())
 
         response = s.recv(1024).decode().strip()
         print("Tracker response:", response)
+
+def client(conn, addr, shared_folder):
+    print("Peer connected:", addr)
+
+    try:
+        command = recv_all(conn)
+        print("Received command:", command)
+
+        parts = command.split()
+
+        if len(parts) != 2 or parts[0] != "GET":
+            conn.sendall(b"ERROR Invalid command\n")
+            return
+
+        filename = parts[1]
+        file_path = os.path.join(shared_folder, filename)
+
+        if not os.path.isfile(file_path):
+            conn.sendall(b"ERROR File not found\n")
+            return
+
+        filesize = os.path.getsize(file_path)
+
+        header = f"OK {filesize}\n"
+        conn.sendall(header.encode())
+
+        with open(file_path, "rb") as f:
+            while True:
+                data = f.read(4096)
+
+                if not data:
+                    break
+
+                conn.sendall(data)
+
+        print("Sent file:", filename)
+
+    finally:
+        conn.close()
+        print("Peer disconnected:", addr)
+
+def server(peer_ip, peer_port, shared_folder):
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+    server_socket.bind((peer_ip, peer_port))
+    server_socket.listen()
+
+    print(f"Peer server listening on {peer_ip}:{peer_port}")
+
+    while True:
+        conn, addr = server_socket.accept()
+
+        client_thread = threading.Thread(
+            target=client,
+            args=(conn, addr, shared_folder)
+        )
+
+        client_thread.start()
 
 def main():
     if len(sys.argv) != 4:
@@ -54,6 +124,7 @@ def main():
     print("Shared folder:", shared_folder)
 
     send_register(peer_id, peer_ip, peer_port, shared_folder)
+    server(peer_ip, peer_port, shared_folder)
 
 
 if __name__ == "__main__":
